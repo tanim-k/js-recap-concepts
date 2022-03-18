4527082401fd681fb462e9471df55cb6ce471a48
tanim-k <tanimkhan877@gmail.com>
2022-03-18 14:28:05 +0600

    closure fn for banking js

---
+++ scope-marriage.js
@@ -0,0 +1,22 @@
+// jodi ami baire variable declare kori: it will work like scope chain:
+const baperTk = 100000;
+
+function PatriChai(age, earning) {
+    console.log(trueAge);
+    var decision = callMarry(15000);
+    console.log(decision)
+    console.log(age);
+    console.log(earning);
+
+    var trueAge = age+ 7;
+
+    function callMarry(expense) {
+        const remaining =baperTk + earning - expense;
+        if(remaining > 1000) {
+            return true;
+        }
+        return false;
+    }
+}
+// fn calling
+PatriChai(21, 50000)
